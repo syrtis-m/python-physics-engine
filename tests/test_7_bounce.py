93ce478593e88115ec21a9c0f1a4a7379efdbbd9
syrtis-m <54552425+syrtis-m@users.noreply.github.com>
2022-10-15 18:05:40 -0500

    update bounce test

--- tests/test_7_bounce.py
+++ tests/test_7_bounce.py
@@ -7,23 +7,28 @@ from scene import *
 from objects.StaticObject import StaticObject
 from objects.ParticleObject import ParticleObject
 from objects.BallObject import BallObject
+from timeit import default_timer as timer
 
 
-def test_7_bounce():
+def test_7_bounce(waittime):
 
     cm = CollisionManager()
     s = scene(cm, 0.5, 0.5, 1)
     
-    #s.create_static_object(StaticObject((3, 255, 37), ((15,15),(0,15))))
+    s.create_static_object(StaticObject((3, 255, 37), ((15,15),(0,15)))) #flat surface
 
-    s.create_static_object(StaticObject((3, 255, 37), ((15,12),(0,15))))
-    s.create_physics_object(BallObject(14,0,(-0.25,0),(255,255,255)))
+    #s.create_static_object(StaticObject((3, 255, 37), ((15,12),(0,15)))) #diagonal surface
+    #s.create_static_object(StaticObject((3, 255, 37), ((15,0),(0,15)))) #test weird surface
+
+    s.create_physics_object(BallObject(14,0,(-0.5,0),(255,255,255)))
     
-
-    while(True):
+    start = timer()
+    end = timer()
+    while((end-start) < waittime):
         s.render()
         time.sleep(0.1)
         s.update()
+        end = timer()
 
 
-test_7_bounce()
+test_7_bounce(5)
